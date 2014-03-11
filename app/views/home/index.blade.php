5fd2e407acbb3f0c9e5e30d62dbea1025eb30173
Artemis <lp859@hotmail.fr>
2014-03-11 16:39:22 +0100

    Edited home page text

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -9,8 +9,12 @@
 		<div class="large-12 columns content">
 			<h2>Deimos</h2>
 			<p>
-			<b>Deimos</b> is an ambitious projet for our freshman year at EPITA. It is basically an old school competitive 3D FPS featuring unique gameplay mechanisms, such as a "mystery" weapon that allows to challenge other players in fun mini-games instead of a classical gun fight.
+				<b>Deimos</b> is our freshman year project at EPITA. It is basically an oldschool competitive 3D FPS featuring unique gameplay mechanisms, such as a "mystery" weapon that allows players to challenge others in fun and different mini-games in addition to the classical fast-paced gun fight.
 			</p>
+			
+
+
+			
 			
 			<div>
 				<ul class="clearing-thumbs" data-clearing>
@@ -18,11 +22,11 @@
 					<li><a href="images/screenshots/img2.jpg" data-no-instant><img src="images/screenshots/img2-th.jpg"></a></li>
 					<li><a href="images/screenshots/img3.jpg" data-no-instant><img src="images/screenshots/img3-th.jpg"></a></li>
 				</ul>
-				<p><em>Deimos game engine featuring a Half-life&reg; map with original textures</em></p>
+				<p><em>Deimos game engine featuring an imported Half-life&reg; map with its original textures</em></p>
 			</div>
 	
 			<p>
-				<b>Deimos</b> is being developped by four members: Manou Manouel (game engine), Cyrilou (visuals/sounds), Uranus (gameplay) & Loupou (server/networking). We have already come a long way during the creation of the game, as the graphical engine is mostly complete, along with working physics, WIP server and miscellaneous gameplay elements.
+				<b>Deimos</b> is being developed by four members: Manou Manouel (Game Engine), Cyrilou (Visuals/Sounds), Uranus (Gameplay) & Loupou (Server/Networking). We have already come a long way in the creation of the game, as the graphical engine is mostly complete, along with working physics, WIP server and core gameplay elements.
 			</p>
 	
 			<p>
