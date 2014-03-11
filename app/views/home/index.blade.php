4bea6cb47670878e761654b4af71c4880afa80ff
Artemis <lp859@hotmail.fr>
2014-03-11 16:14:00 +0100

    Added a description for images + added a real link for the news in the main page text

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -12,20 +12,21 @@
 			<b>Deimos</b> is an ambitious projet for our freshman year at EPITA. It is basically an old school competitive 3D FPS featuring unique gameplay mechanisms, such as a "mystery" weapon that allows to challenge other players in fun mini-games instead of a classical gun fight.
 			</p>
 			
-			<p>
-				<ul class="clearing-thumbs" align="center" data-clearing>
+			<div>
+				<ul class="clearing-thumbs" data-clearing>
 					<li><a href="images/screenshots/img1.jpg" data-no-instant><img src="images/screenshots/img1-th.jpg"></a></li>
 					<li><a href="images/screenshots/img2.jpg" data-no-instant><img src="images/screenshots/img2-th.jpg"></a></li>
 					<li><a href="images/screenshots/img3.jpg" data-no-instant><img src="images/screenshots/img3-th.jpg"></a></li>
 				</ul>
-			</p>
+				<p><em>Deimos game engine featuring a Half-life&reg; map with original textures</em></p>
+			</div>
 	
 			<p>
 				<b>Deimos</b> is being developped by four members: Manou Manouel (game engine), Cyrilou (visuals/sounds), Uranus (gameplay) & Loupou (server/networking). We have already come a long way during the creation of the game, as the graphical engine is mostly complete, along with working physics, WIP server and miscellaneous gameplay elements.
 			</p>
 	
 			<p>
-				<em><a href="#">Stay tuned for more news about Deimos! &#8594;</a></em>
+				<em><a href="{{ URL::action('NewsController@getIndex') }}">Stay tuned for more news about Deimos! &#8594;</a></em>
 			</p>
 		</div>
 	</div>
