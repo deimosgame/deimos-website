c90db08efe52ee44dce503fbf736377bf46f8105
Manuel HUEZ <manuel.huez@gmail.com>
2014-03-12 01:02:03 +0100

    Added google description.

--- app/views/template/default.blade.php
+++ app/views/template/default.blade.php
@@ -7,6 +7,8 @@
         <title>Deimos - @yield('pageName')</title>
         {{ HTML::style("//fonts.googleapis.com/css?family=Lato:300, 500, 700") }}
         {{ HTML::style("css/app.css") }}
+
+        <meta name="description" content="Deimos is a 3D retro FPS game, created by 4 students of EPITA. Try it, you won't regret it, we promess!" />
     </head>
     <body>
 
